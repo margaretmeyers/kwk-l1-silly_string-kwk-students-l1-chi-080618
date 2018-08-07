3a85d4f291cfd6f5f11bf633ea9edb066b5f8036
margaretmeyers <marg.m.meyers@gmail.com>
2018-08-07 14:05:25 +0000

    automatically backed up by learn

--- silly_string.rb
+++ silly_string.rb
@@ -1,21 +1,28 @@
 
 # Challenge 1: Fill this string in with your favorite word
 
-your_favorite_word = ""
+your_favorite_word = "cereal"
 
 puts "My favorite word is #{your_favorite_word}"
 
+
 # Challenge 2: Capitalize every letter in your_favorite_word using a built in Ruby method
 
-favorite_word_in_all_caps = your_favorite_word # Put a . after your_favorite word and use a String method.
+favorite_word_in_all_caps = your_favorite_word.upcase 
 
-puts "MY FAVORITE WORD IS #{favorite_word_in_all_caps}"
+
+
+# Put a . after your_favorite word and use a String method.
+
+puts "my favorite word is #{your_favorite_word}"
 
 # Challenge 3: Make all the letters of phrase lowercase using a built in Ruby method
 
 phrase = "IM NOT SHOUTING"
 
-lowercased_phrase = phrase # Put a . after phrase and use a String method.
+lowercased_phrase = phrase.downcase
+
+# Put a . after phrase and use a String method.
 
 puts lowercased_phrase
 
@@ -23,21 +30,25 @@ puts lowercased_phrase
 
 big_word = "supercalifragilisticexpialidocious"
 
-letter_count = big_word # Put a . after big_word and use a String method.
+letter_count = big_word.length
 
-puts "There are #{letter_count} in #{big_word}"
+# Put a . after big_word and use a String method.
+
+puts "There are #{letter_count} letters in #{big_word}"
 
 # Challenge 5: Add "wow" and "mom" to big_word
 
-bigger_word = big_word # Use String concatenation to add "wow" and "mom" to big_word
+bigger_word = big_word + "wow" + "mom"
 
-new_letter_count = bigger_word.size
+# Use String concatenation to add "wow" and "mom" to big_word
+
+new_letter_count = bigger_word.length
 
 puts "There are now #{new_letter_count} in #{bigger_word}"
 
 # Challenge 6: Capitalize this sentence properly
 
-proper_sentence = "i really like programming." # Put a . after the string to capitalize it properly.
+proper_sentence = "i really like programming.".capitalize # Put a . after the string to capitalize it properly.
 
 puts proper_sentence
 
